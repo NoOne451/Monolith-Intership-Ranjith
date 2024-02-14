c7ea11eb53740892a69c23c4ffa0a2e53b121bbd
NoOne451 <ranjithdurai111@gmail.com>
2024-02-14 14:09:10 +0530

    card design with angular

--- dist/helloworld/server/main.server.mjs
+++ dist/helloworld/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5DN333SG.mjs";import"./chunk-2P4S7PAE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-Q7563IVJ.mjs";import"./chunk-2P4S7PAE.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
